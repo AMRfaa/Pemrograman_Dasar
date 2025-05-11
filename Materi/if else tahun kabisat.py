e49c3f7410574e33b6035eda02e20cf9a18ada5c
Anisa Muziya Rafa <amrxll08@gmail.com>
2025-05-11 12:51:09 +0800

    Add files via upload

---
+++ Materi/if else tahun kabisat.py
@@ -0,0 +1,6 @@
+# menentukan apakah usia anda sudah mencapai usia minimal buat ktp atau belum
+usia = int(input("masukan usia anda : "))
+if usia >= 18 :
+    print ("anda memenuhi syarat usia minimal buat ktp")
+else :
+    print ("belum memenuhi syarat usia minimal buat ktp")
